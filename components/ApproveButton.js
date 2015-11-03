4551e84f79b1ed0cf2322481a2f72c5f1143a75f
Arnaud Benard <arnaudbenard13@gmail.com>
2015-11-03 15:25:52 +0000

    revert shallow buttons components to normal ones until we find a way to properly test them

--- components/ApproveButton.js
+++ components/ApproveButton.js
@@ -2,17 +2,22 @@ import React, { Component, PropTypes } from 'react';
 import AsyncButton from './AsyncButton';
 import Icon from './Icon';
 
-const ApproveButton = ({approveTransaction, inProgress}) => {
-  return (
-    <div>
-      <AsyncButton
-        customClass='Button--approve'
-        inProgress={inProgress}
-        onClick={approveTransaction.bind(this)}>
-        <Icon type='approved' /> Approve
-      </AsyncButton>
-    </div>
-  );
+// Leave non shallow until proper way of testing them
+class ApproveButton extends Component {
+  render() {
+    const { approveTransaction, inProgress } = this.props;
+
+    return (
+      <div>
+        <AsyncButton
+          customClass='Button--approve'
+          inProgress={inProgress}
+          onClick={approveTransaction.bind(this)}>
+          <Icon type='approved' /> Approve
+        </AsyncButton>
+      </div>
+    );
+  }
 }
 
 ApproveButton.propTypes = {
--- components/RejectButton.js
+++ components/RejectButton.js
@@ -2,17 +2,22 @@ import React, { Component, PropTypes } from 'react';
 import AsyncButton from './AsyncButton';
 import Icon from './Icon';
 
-const RejectButton = ({rejectTransaction, inProgress}) => {
-  return (
-    <div>
-      <AsyncButton
-        customClass='Button--reject'
-        inProgress={inProgress}
-        onClick={rejectTransaction.bind(this)}>
-        <Icon type='rejected' /> Reject
-      </AsyncButton>
-    </div>
-  );
+// Leave non shallow until proper way of testing them
+class RejectButton extends Component {
+  render() {
+    const {rejectTransaction, inProgress} = this.props;
+
+    return (
+      <div>
+        <AsyncButton
+          customClass='Button--reject'
+          inProgress={inProgress}
+          onClick={rejectTransaction.bind(this)}>
+          <Icon type='rejected' /> Reject
+        </AsyncButton>
+      </div>
+    );
+  }
 }
 
 RejectButton.propTypes = {
