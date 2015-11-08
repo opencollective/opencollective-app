e8183d585821d49e2d693af160108907bc7da336
Fernando Montoya <montogeek@gmail.com>
2015-11-07 23:57:28 -0500

    Fix imports routes

--- src/store/create.js
+++ src/store/create.js
@@ -4,7 +4,7 @@ import createHistory from 'history/lib/createBrowserHistory';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import * as reducers from '../reducers/index';
-import routes from '../routes';
+import routes from '../../routes';
 
 const combinedReducers = combineReducers(reducers);
 const logger = createLogger();
