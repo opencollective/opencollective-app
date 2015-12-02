2df93cc11f55f0fc69f1ff6def01f44a4315db07
Arnaud Benard <arnaudbenard13@gmail.com>
2015-12-02 13:23:50 +0100

    added test for group donation

--- actions/groups/donate.js
+++ actions/groups/donate.js
@@ -5,31 +5,30 @@ import * as constants from '../../constants/groups';
  * Donate to a group
  */
 
-export default (groupid, payment) => {
-  const url = `groups/${groupid}/payments/`;
+export default (id, payment) => {
+  const url = `groups/${id}/payments/`;
 
   return dispatch => {
-    console.log('request', groupid, payment);
-    dispatch(request(groupid, payment));
+    dispatch(request(id, payment));
     return postJSON(url, {payment})
-      .then(json => dispatch(success(groupid, json)))
+      .then(json => dispatch(success(id, json)))
       .catch(error => dispatch(failure(error)));
   };
 };
 
-function request(groupid, payment) {
+function request(id, payment) {
   return {
     type: constants.DONATE_GROUP_REQUEST,
-    groupid,
+    id,
     payment
   };
 }
 
-function success(groupid, payment) {
+function success(id, payment) {
 
   return {
     type: constants.DONATE_GROUP_SUCCESS,
-    groupid,
+    id,
     payment
   };
 }
