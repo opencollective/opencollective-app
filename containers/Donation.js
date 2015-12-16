56c3df3aee5c5669bba07557113bd304e6886e92
Xavier Damman <xdamman@gmail.com>
2015-12-15 18:20:53 -0800

    Fix for tests passing

--- containers/Donation.js
+++ containers/Donation.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { pushState } from 'redux-router';
-import values from 'lodash/object/values';
 import getUniqueValues from '../lib/get_unique_values';
 
 import rejectError from '../lib/reject_error';
