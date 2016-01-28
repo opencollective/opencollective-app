55eb13d92fa410b864eefd0ffc498c8368849c00
Xavier Damman <xdamman@gmail.com>
2016-01-27 18:16:17 -0800

    Adding the host in the list of members

--- components/UsersList.js
+++ components/UsersList.js
@@ -15,11 +15,13 @@ export default ({users=[]}) => {
 
   return (
     <div className='UsersList'>
-      {users.map(({id, avatar, name, website, twitterHandle}) => {
+      {users.map(({id, avatar, name, role, website, twitterHandle}) => {
         const href = website || (twitterHandle) ? 'https://twitter.com/' + twitterHandle : null;
+        let title = name + ' is ';
+        title += (role === 'HOST') ? 'the host' : 'a member';
         return (
             <div className='UsersList-item' key={id}>
-              {href ? <a href={href} title={name}>{profileImageName(avatar,name)}</a> : profileImageName(avatar,name) }
+              {href ? <a href={href} title={title}>{profileImageName(avatar,name)}</a> : profileImageName(avatar,name) }
             </div>
         );
       })}
