e866431133eddeced521ef76a9ce6a8098796635
Xavier Damman <xdamman@gmail.com>
2016-01-27 17:24:33 -0800

    Added a link to app.opencollective.com

--- components/PublicTopBar.js
+++ components/PublicTopBar.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 export default () => {
   return (
-    <a href="https://opencollective.com#apply">
-      <div className='PublicTopBar'>
-        <div className='PublicTopBar-tagLine'>
-          <div className='OC-Icon'>
-            <i className='Icon Icon--oc' />
-          </div>
-          <div className="Tagline">
-            Join OpenCollective to start collecting funds
-          </div>
+    <div className='PublicTopBar'>
+      <div className='PublicTopBar-tagLine'>
+        <div className='OC-Icon'>
+          <i className='Icon Icon--oc' />
         </div>
-        <div className='PublicTopBar-signup'>
+        <div className="Tagline">
+          <a href="https://opencollective.com#apply">
+            Join OpenCollective to start collecting funds
+          </a>
         </div>
       </div>
-    </a>
+      <div className='PublicTopBar-signup'>
+        <a href="https://app.opencollective.com">Sign in</a>
+      </div>
+    </div>
   );
 };
