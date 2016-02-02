b5b01d200f907aa859de4e94f8a6bdd0fab9d99e
Xavier Damman <xdamman@gmail.com>
2016-02-01 23:43:05 -0800

    Right color for footer links

--- components/PublicFooter.js
+++ components/PublicFooter.js
@@ -5,7 +5,7 @@ export default () => {
     <div className='PublicFooter'>
       <img src='/static/images/logo-grey.svg' />
       <div className='PublicFooter-menu'>
-        <div className='PublicFooter-menuItem'><a href="https://opencollective.com/FAQ">FAQ</a></div>
+        <div className='PublicFooter-menuItem'><a href="https://opencollective.com/faq">FAQ</a></div>
         <div className='PublicFooter-menuItem'><a href="mailto:info@opencollective.com">Contact</a></div>
       </div>
     </div>
