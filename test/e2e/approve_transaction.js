700aed3ca74593ece0a6e3177d2e5feb868677d7
Xavier Damman <xdamman@gmail.com>
2016-03-27 23:35:47 -0400

    also testing manual expense

--- test/e2e/approve_transaction.js
+++ test/e2e/approve_transaction.js
@@ -1,8 +1,3 @@
-const transaction = {
-  description: `Day out in tahoe ${Math.random()}`,
-  amount: 10
-};
-
 module.exports = {
   '@tags': ['approve_transaction'],
   before: (client) => {
@@ -51,26 +46,51 @@ module.exports = {
       .click('#saveBtn')
   },
   
-  'submit a new expense': (client) => {
+  'submit a new expense (already reimbursed)': (client) => {
     client
       .url('http://localhost:3000/groups/1/transactions/new')
-      .setValue('.js-transaction-description input', transaction.description)
-      .setValue('.js-transaction-amount input', transaction.amount)
+      .setValue('.js-transaction-description input', 'Expense already reimbursed')
+      .setValue('.js-transaction-amount input', 20)
+      .waitForElementVisible("option[value='manual']", 1000)
+      .click("option[value='manual']")
+      .submitForm('form.TransactionForm-form')
+      .waitForElementVisible('.Transaction', 1500)
+      .assert.containsText('.Transaction:first-child', 'Pending')
+  },
+
+  'approve an expense already reimbursed (manual)': (client) => {
+    client
+      .click('.Transaction')
+      .waitForElementVisible('.Button--approve', 1000)
+      .assert.containsText('.Button--approve', 'APPROVE')
+      .click('.Button--approve')
+      .pause(1000)
+      .waitForElementVisible('.Transaction', 5000)
+      .pause(1000) // wait for transaction
+      .assert.urlEquals('http://localhost:3000/groups/1/transactions')
+      .assert.containsText('.Transaction:first-child', 'Reimbursed')
+  },
+
+  'submit a new expense to be reimbursed': (client) => {
+    client
+      .url('http://localhost:3000/groups/1/transactions/new')
+      .setValue('.js-transaction-description input', 'Expense to be reimbursed via PayPal')
+      .setValue('.js-transaction-amount input', 5)
       .waitForElementVisible("option[value='paypal']", 1000)
       .click("option[value='paypal']")
       .submitForm('form.TransactionForm-form')
       .waitForElementVisible('.Transaction', 1500)
-      .waitForElementVisible('.Export-link', 1500);    
+      .waitForElementVisible('.Export-link', 1500)
   },
 
-  'approve an expense': (client) => {
+  'approve and pay an expense via paypal': (client) => {
     client
       .click('.Transaction')
       .waitForElementVisible('.Button--approve', 1000)
-      .assert.containsText('body', 'APPROVE')
-      .click('.Button--approve')
-      .pause(1000)
-      .waitForElementVisible('.Transaction', 5000)
+      .assert.containsText('.Button--approve', 'APPROVE AND PAY')
+      .click('.Button--approve') // this may take some time as it is calling paypal sandbox
+      .pause(2000)
+      .waitForElementVisible('.Transaction', 10000)
       .pause(1000) // wait for transaction
       .assert.urlEquals('http://localhost:3000/groups/1/transactions')
       .assert.containsText('.Transaction:first-child', 'Reimbursed')
