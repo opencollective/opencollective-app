82c8b25e594b31c51d6a4ffecf62576b264c2c12
Aseem Sood <asood123@yahoo.com>
2016-04-14 10:56:57 -0400

    removing title check and adding a success check in reset_db

--- test/lib/reset_db.js
+++ test/lib/reset_db.js
@@ -3,6 +3,5 @@ const config = require('config');
 module.exports = client => {
   return client
     .url(`${config.host.api}/database/reset`)
-    // TODO this isn't enough, can still ignore errors
-    .assert.title("", `checking ${config.host.api}/database/reset didn't return error`);
+    .assert.containsText('body', '\"success\":true');
 };
