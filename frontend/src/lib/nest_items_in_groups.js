65fd281ef60bbf66e9af4123523e7e4cb5aede03
Sébastien Dubois <sedubois@users.noreply.github.com>
2016-09-16 04:10:00 +0300

    Remove approved transaction field (#195)

--- frontend/src/lib/nest_items_in_groups.js
+++ frontend/src/lib/nest_items_in_groups.js
@@ -7,7 +7,7 @@ import sortByDate from './sort_by_date';
 import { PENDING, APPROVED } from '../constants/expenses';
 
 /**
- * Builds the following structure from a group collection and a transaction
+ * Builds the following structure from a group collection and an expense
  * collection. This is used in the UI to display the data
  *
  * @example
@@ -15,22 +15,11 @@ import { PENDING, APPROVED } from '../constants/expenses';
  * => [{
  *      id: 1,
  *      name: 'Group1',
- *      transactions: [{ id: 1, GroupId: 1 }]
+ *      expenses: [{ id: 1, GroupId: 1 }]
  *    }]
  */
 
 export default {
-  nestTransactionsInGroups: (groups, transactions) => {
-    const transactionsArray = values(transactions);
-    const groupsArray = values(groups);
-
-    return groupsArray.map((group) => {
-      return extend(group, {
-        transactions: filter(transactionsArray, { GroupId: group.id, approved: false }).sort(sortByDate)
-      });
-    });
-  },
-
   nestExpensesInGroups: (groups, expenses) => {
     const expensesArray = values(expenses);
     const groupsArray = values(groups);
